d27215e23b569d5752babfdf287f82de21fb6223
wassimbj <wassimbenjdida@gmail.com>
2024-11-04 10:27:21 +0100

    fix build

--- build.sh
+++ build.sh
@@ -3,26 +3,29 @@
 zip_name="todoit@wassimbj.github.io"
 
 # the container of the final running build
-echo "Create build dir"
+echo "######## Create build dir ########"
 mkdir build
 
 # to generate the *.js files required to run the extension
-echo "Build ts"
+echo "######## Build ts ########"
 yarn build
 
 # build the schema
-echo "Build schema"
+echo "######## Build schema ########"
 make schemas
 
 # copy the required files
-echo "Copy required files"
+echo "######## Copy required files ########"
 cp -r schemas build
 cp *.js build
 cp stylesheet.css build
 cp metadata.json build
 
-echo "Zip"
-zip $zip_name.zip -x *.git -r build
+echo "######## Zip ########"
+cd build
+zip ../$zip_name.zip  -x *.git -r .
 
-tree build
+
+echo "######## Final result ########"
+tree .
 
